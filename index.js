33132dd3386c6ce8ae4bcdc888b0dd630fda2dc3
Confidence Okoghenun <confidenceboi@gmail.com>
2019-07-05 23:39:49 +0100

    feat: Added full page zoom mode

--- index.js
+++ index.js
@@ -1,37 +1,75 @@
-let productPhotoArr = ['product1.jpg', 'product2.jpg', 'product3.jpg', 'product4.jpg', 'product5.jpg'];
-    let imgContainer = document.querySelector('.view-finder .images');
-    imgContainer.innerHTML = '';
-    let productDisplay = document.querySelector('.display-with-controls .display');
-    let currentPhotoIndex = 0;
-    let img;
-    let imgControls = document.querySelectorAll('.controls img');
+let productPhotoArr = [
+  "img/product1.jpg",
+  "img/product2.jpg",
+  "img/product3.jpg",
+  "img/product4.jpg",
+  "img/product5.jpg"
+];
+let imgContainer = document.querySelector(".view-finder .images");
+imgContainer.innerHTML = "";
+let productDisplay = document.querySelector(".display-with-controls .display");
+const productDisplayContainer = document.querySelector('.display-with-controls');
+let currentPhotoIndex = 0;
+let img;
+// let imgControls = document.querySelector(".controls");
+const imgPopupCloseBtn = document.querySelector(".img-popup-close-btn");
+const imgPopupModal = document.querySelector(".img-popup");
+const imgPopupModalImg = document.querySelector(".img-popup-container-img");
 
-    productPhotoArr.forEach(function (img, i) {
-        if (i == 0) {
-            imgContainer.innerHTML += `<img src="img/${img}" alt="" class="active">`
-        } else {
-            imgContainer.innerHTML += `<img src="img/${img}" alt="">`
-        }
-    })
-    imgControls.forEach(function (control) {
-        control.addEventListener('click', function (e) {
-            if (e.target.getAttribute('alt') === 'Next') {
-                currentPhotoIndex >= productPhotoArr.length - 1 ? currentPhotoIndex = 0 : currentPhotoIndex++
-                img = productPhotoArr[currentPhotoIndex];
-                productDisplay.style.backgroundImage = `url(img/${img})`;
-            } else {
-                currentPhotoIndex == 0 ? currentPhotoIndex = productPhotoArr.length - 1 : currentPhotoIndex--
-                img = productPhotoArr[currentPhotoIndex];
-                productDisplay.style.backgroundImage = `url(img/${img})`;
+setBackgroundImage(0);
 
-            }
+productPhotoArr.forEach(function(img, i) {
+  if (i == 0) {
+    imgContainer.innerHTML += `<div style="
+    background-image: url(${img});" alt="" class="active image-item" data-index="${i}"></div>`;
+  } else {
+    imgContainer.innerHTML += `<div style="
+    background-image: url(${img});" alt="" class="image-item" data-index="${i}"></div>`;
+  }
+});
 
-            document.querySelectorAll('.view-finder .images img').forEach(function (img, i) {
-                if (i == currentPhotoIndex) {
-                    img.classList.add('active')
-                } else {
-                    img.classList.remove('active')
-                }
-            })
-        })
-    })
+productDisplayContainer.addEventListener("click", function(e) {
+  if (e.target.getAttribute("alt") === "Next") {
+    currentPhotoIndex >= productPhotoArr.length - 1
+      ? (currentPhotoIndex = 0)
+      : currentPhotoIndex++;
+    setBackgroundImage(currentPhotoIndex);
+  } else if (e.target.getAttribute("alt") === "Previous") {
+    currentPhotoIndex == 0
+      ? (currentPhotoIndex = productPhotoArr.length - 1)
+      : currentPhotoIndex--;
+    setBackgroundImage(currentPhotoIndex);
+  } else {
+    showImgPopupModal();
+  }
+});
+
+imgContainer.querySelectorAll(".image-item").forEach(img => {
+  img.addEventListener("click", e => {
+    setBackgroundImage(e.target.getAttribute("data-index"));
+  });
+});
+
+function showImgPopupModal() {
+    imgPopupModal.classList.remove("hide");
+}
+
+function setBackgroundImage(imgIndex) {
+  img = productPhotoArr[imgIndex];
+  productDisplay.style.backgroundImage = `url(${img})`;
+  imgPopupModalImg.style.backgroundImage = `url(${img})`;
+
+  document
+    .querySelectorAll(".view-finder .images .image-item")
+    .forEach(function(img, i) {
+      if (i == imgIndex) {
+        img.classList.add("active");
+      } else {
+        img.classList.remove("active");
+      }
+    });
+}
+
+imgPopupCloseBtn.addEventListener("click", () => {
+  imgPopupModal.classList.add("hide");
+});
